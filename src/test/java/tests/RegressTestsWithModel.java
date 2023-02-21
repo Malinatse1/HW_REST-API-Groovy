5e89237f3214afd539756595fab2d227be26c174
Natalya Bochkova <bochkovanatalya7@gmail.com>
2023-02-21 12:42:41 +0300

    add tests

--- src/test/java/tests/RegressTestsWithModel.java
+++ src/test/java/tests/RegressTestsWithModel.java
@@ -1,21 +1,21 @@
-import io.restassured.http.ContentType;
-import lombok.RequestData;
-import lombok.ResponseData;
+package tests;
+import lombok.ResponseDataModel;
+import models.RequestDataModel;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-public class RegressTests {
+
+public class RegressTestsWithModel {
     TestData testData = new TestData();
 
     @Test
     void createUserTest() {
-        RequestData data = new RequestData();
+        RequestDataModel data = new RequestDataModel();
         data.setName(testData.firstName);
         data.setJob(testData.job);
-        ResponseData responseData = Specs.request
+        ResponseDataModel response = Specs.request
                 .body(data)
                 .when()
                 .post("/users")
@@ -23,17 +23,17 @@ public class RegressTests {
                 .log().status()
                 .log().body()
                 .statusCode(201)
-                .extract().as(ResponseData.class);
-        assertEquals(responseData.getName(), testData.firstName);
-        assertEquals(responseData.getJob(), testData.job);
+                .extract().as(ResponseDataModel.class);
+        assertEquals(response.getName(), testData.firstName);
+        assertEquals(response.getJob(), testData.job);
     }
 
 
     @Test
     void updateUserTestWithLombok() {
-        RequestData data = new RequestData();
+        RequestDataModel data = new RequestDataModel();
         data.setName("Alfred");
-        ResponseData responseData = Specs.request
+        ResponseDataModel response = Specs.request
                 .body(data)
                 .when()
                 .patch("/users/447")
@@ -41,13 +41,13 @@ public class RegressTests {
                 .log().status()
                 .log().body()
                 .spec(Specs.responseSpec)
-                .extract().as(ResponseData.class);
-        assertEquals(responseData.getName(),("Alfred"));
+                .extract().as(ResponseDataModel.class);
+        assertEquals(response.getName(),("Alfred"));
     }
 
     @Test
     void deleteUserTest() {
-        RequestData data = new RequestData();
+        RequestDataModel data = new RequestDataModel();
         data.setName(testData.firstName);
         data.setJob(testData.job);
         Specs.request
@@ -76,8 +76,6 @@ public class RegressTests {
                 .when()
                 .get("/users")
                 .then().log().body()
-//                .spec(Specs.responseSpec)
-//                .body("data.id", is(7));
                 .body("data.findAll{it.id == 3}.last_name", hasItem("Wong"))
                 .body("data.findAll{it.id == 3}.first_name", hasItem("Emma"))
                 .body("data.findAll{it.id == 3}.email", hasItem("emma.wong@reqres.in"));
