5920fe8db257e6f7e3f3e6e4a6f56c03e25b3ca1
Natalya Bochkova <bochkovanatalya7@gmail.com>
2023-02-22 18:17:18 +0300

    fix tests

--- src/test/java/lombok/RequestData.java
+++
@@ -1,9 +0,0 @@
-package lombok;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-@Data
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class RequestData {
-    public String name, job;
-    public Integer total,id;
-}
