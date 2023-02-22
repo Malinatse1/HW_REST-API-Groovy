2b7306f04d9b95d3b61cc84f74a210e383c1d04a
Natalya Bochkova <bochkovanatalya7@gmail.com>
2023-02-22 18:01:43 +0300

    fix tests

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,16 @@
+package tests;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+import static helpers.CustomApiListener.withCustomTemplates;
+
+public class TestBase {
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.filters(new AllureRestAssured());
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
+}
