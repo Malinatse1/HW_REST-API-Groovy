cf2a13d33e4515fac0a083c508936a626bd6f0ba
Natalya Bochkova <bochkovanatalya7@gmail.com>
2023-03-05 20:18:10 +0300

    fix tests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,9 +5,9 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
 
-public class TestBase {
-    @BeforeAll
-    public static void setUp() {
-        RestAssured.filters(new AllureRestAssured());
-    }
-}
+//public class TestBase {
+//    @BeforeAll
+////    public static void setUp() {
+////        RestAssured.filters(new AllureRestAssured());
+////    }
+
