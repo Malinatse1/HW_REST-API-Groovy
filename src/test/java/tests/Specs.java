f773280f3c97245789199533c825c141b71a3d98
Natalya Bochkova <bochkovanatalya7@gmail.com>
2023-03-05 20:24:39 +0300

    fix tests

--- src/test/java/tests/Specs.java
+++ src/test/java/tests/Specs.java
@@ -3,8 +3,6 @@ package tests;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-
-import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,8 @@
 package tests;
 
-import io.qameta.allure.restassured.AllureRestAssured;
+
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
-
 import static helpers.CustomApiListener.withCustomTemplates;
 
 
